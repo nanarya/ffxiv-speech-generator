4a3accda7d52b794ebeb9fcaac83b77e5f26dfab
人見建也 <hitomi@i-fc.jp>
2019-01-23 18:01:29 +0900

    #4 ある程度調整。

--- src/components/Imager.vue
+++ src/components/Imager.vue
@@ -23,35 +23,28 @@ export default {
   props: {
     name: {
       type: String,
-      default: 'てきすと'
+      default: 'おひげ'
     },
     message: {
       type: String,
-      default: 'ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。'
+      default: 'がおー！'
     },
     messageBoxSrc: {
       type: String,
       default: '/static/fukidashi02.png'
     }
   },
-  // watch: {
-  //   src () {
-  //     this.loadLocalImage()
-  //     this.draw()
-  //   }
-  // },
   mounted () {
     this.file = document.getElementById('file')
     this.result = document.getElementById('result')
-
     this.canvas = this.$el.querySelector('#canvas')
     this.ctx = this.canvas.getContext('2d')
 
     // canvas上に画像を表示
     this.img = new Image()
+    this.img.src = '/static/default.jpg'
     this.messageBoxImage = new Image()
     this.messageBoxImage.src = this.messageBoxSrc
-    this.img.src = 'https://www.tam-tam.co.jp/tipsnote/wpdata/wp-content/uploads/2017/10/canvas_image.jpg'
     this.canvasDraw()
   },
   methods: {
@@ -62,61 +55,100 @@ export default {
     onMsgChange () {
       this.canvasDraw()
     },
-    loadLocalImage (file) {
-      this.fileData = file
-
-      if (!this.fileData.type.match('image.*')) {
+    loadLocalImage (fileData) {
+      if (!fileData.type.match('image.*')) {
         alert('画像を選択してください')
         return
       }
       this.reader = new FileReader()
+      this.img = new Image()
       this.reader.onloadend = () => {
-        this.img = new Image()
-        this.img.onload = function () {
-          console.log(this.img)
+        this.img.onload = () => {
+          this.canvasDraw()
         }
         this.img.src = this.reader.result
-        console.log(this.img.naturalWidth)
-
-        // console.log(this.img.naturalHeight)
-        // this.result.appendChild(this.img)
-        this.canvasDraw()
       }
       // 読み込みの実行
-      this.reader.readAsDataURL(this.fileData)
+      this.reader.readAsDataURL(fileData)
     },
     canvasDraw () {
-      // console.log(this.img.naturalHeight) // => だめ
-      this.ctx.clearRect(0, 0, 300, 150)
       let image = new Image()
       image.onload = () => {
-        this.ctx.drawImage(image, 0, 0, 300, 150)
-        this.ctx.drawImage(this.messageBoxImage, 0, 0, 300, 150)
-        this.addText()
+        this.getSize(image)
 
-        // console.log(this.canvas) // => ok
-        // let data = this.canvas.toDataURL('image/png')
-        // let outputImg = document.createElement('img')
-        // outputImg.src = data
-        // console.log(data)
-        // this.result.appendChild(outputImg)
+        // canvas の初期化
+        this.canvas.width = this.imageWidth
+        this.canvas.height = this.imageHeight
+        this.ctx.clearRect(0, 0, this.imageWidth, this.imageHeight)
+
+        this.ctx.drawImage(image, 0, 0, this.imageWidth, this.imageHeight)
+        this.ctx.drawImage(
+          this.messageBoxImage,
+          this.messageBoxPosition.x,
+          this.messageBoxPosition.y,
+          this.messageBoxPosition.w,
+          this.messageBoxPosition.h
+        )
+        this.addText()
       }
       image.src = this.img.src
     },
+    getSize (image) {
+      let width = image.naturalWidth
+      let height = image.naturalHeight
+
+      let ratio = width / height
+      this.imageWidth = Math.min(width, 1000)
+      this.imageHeight = this.imageWidth / ratio
+
+      this.messageBoxPosition = {
+        x: (this.imageWidth / 2) - (this.imageHeight * (180 / 900) * (657 / 158) / 2),
+        y: this.imageHeight * (715 / 900),
+        w: this.imageHeight * (180 / 900) * (657 / 158),
+        h: this.imageHeight * (180 / 900)
+      }
+
+      this.namePosition = {
+        x: this.messageBoxPosition.x + (this.messageBoxPosition.w * 0.08),
+        y: this.imageHeight * (737 / 900),
+        size: Math.round(this.imageHeight * 0.023)
+      }
+
+      this.messagePosition = {
+        x: this.namePosition.x,
+        y: this.imageHeight * (777 / 900),
+        size: this.namePosition.size
+      }
+
+      console.log('=====================')
+      console.log(this.namePosition.size)
+      console.log('=====================')
+
+      console.log(ratio)
+      console.log(this.imageWidth)
+      console.log(this.imageHeight)
+
+      console.log('this.messageBoxPosition')
+      console.log(this.messageBoxPosition)
+      console.log('this.namePosition')
+      console.log(this.namePosition)
+      console.log('this.messagePosition')
+      console.log(this.messagePosition)
+    },
     addText () {
-      let nameFontSize = '20px'
-      let messageFontSize = '20px'
+      let nameFontSize = this.namePosition.size + 'px'
+      let messageFontSize = this.messagePosition.size + 'px'
 
       this.ctx.textAlign = 'left'
       this.ctx.textBaseline = 'middle'
 
       this.ctx.fillStyle = '#FFFFFF'
       this.ctx.font = `normal ${nameFontSize} 'Noto Sans JP'`
-      this.ctx.fillText(this.name, 20, 25)
+      this.ctx.fillText(this.name, this.namePosition.x, this.namePosition.y)
 
-      this.ctx.fillStyle = '#002B69'
+      this.ctx.fillStyle = '#000000'
       this.ctx.font = `normal ${messageFontSize} 'Noto Sans JP'`
-      this.ctx.fillText(this.message, 80, 100)
+      this.ctx.fillText(this.message, this.messagePosition.x, this.messagePosition.y)
     }
   }
 }
